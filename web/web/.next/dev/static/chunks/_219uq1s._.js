038ce78fd94684d246a19c1c5036dceb24470c42
koraxd <bergariusz@gmail.com>
2026-09-16 12:05:44 +0100

    chore: added web to gitignore

--- web/web/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0ph-8z8._.css","static/chunks/node_modules_0f-dkli._.js","static/chunks/src_1echj9y._.js","static/chunks/node_modules_0--sow4._.js","static/chunks/src_11omzug._.js"],
-    source: "entry"
-});

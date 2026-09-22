a07f6d2b029d010407bf08eed8e280d192fce1f0
koraxd <bergariusz@gmail.com>
2026-09-22 06:28:17 +0100

    fix: sorting jobs based on sortField and sortOrder didnt work - added those as dependencies inside useMemo

--- web/src/components/features/applications/ApplicationTableBody.tsx
+++ web/src/components/features/applications/ApplicationTableBody.tsx
@@ -61,7 +61,7 @@ export default function ApplicationTableBody({
 
                 return sortOrder === "asc" ? comparison : -comparison
             }),
-        [filteredJobs]
+        [filteredJobs, sortField, sortOrder]
     )
 
     return (
